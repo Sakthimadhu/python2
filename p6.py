19fb63a38aad5382186f169a30912eebb936566b
Sakthimadhu <51222816+Sakthimadhu@users.noreply.github.com>
2019-07-12 04:20:52 -0700

    Create p6.py

---
+++ p6.py
@@ -0,0 +1,10 @@
+x1=input()
+if((x1>='a' and x1<='z') or (x1>='A' and x1<='Z')):
+  x1=x1.lower()
+  if(x1=='a' or x1=='e' or x1=='i' or x1=='o' or x1=='u'):
+     print("Vowel")
+  else:
+     print("Consonant")
+else:
+    print("Special Characters")
+    
